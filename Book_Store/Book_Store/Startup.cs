347c1931d2c4ca7751ad1d9a8b73606306313313
Sandip300300 <sandip300300@gmail.com>
2022-04-22 22:17:58 +0600

    Added mvc to application

--- Book_Store/Book_Store/Startup.cs
+++ Book_Store/Book_Store/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,7 +23,8 @@ namespace Book_Store
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            // services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,10 +34,7 @@ namespace Book_Store
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
+           
 
             app.UseStaticFiles();
 
@@ -45,7 +44,7 @@ namespace Book_Store
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
---
+++ C_Sharpe_Project_Practice/C_Sharpe_Project_Practice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace C_Sharpe_Project_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 2, 3, 4 };
+            foreach(int item in a)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+        }
+    }
+}
